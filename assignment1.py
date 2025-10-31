d15560da5a743dfd700d46e1f43fc8dd079f093e
dorukyavuz <doruk.yavuz@std.yeditepe.edu.tr>
2025-10-31 14:24:15 +0300

    Added a function called find_period

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def find_period(L0, L1):
+    
+    g = 9.81
+    
+    if L1 > L0 > 0:
+        for L in range(L0, L1 + 1):
+            T = 2 * np.pi * np.sqrt(L / g)
+            print("When L = %4.1f m, T = %3.1f s\n" % (L, T))
+    
+    T0 = 2 * np.pi * np.sqrt(L0 / g)
+    T1 = 2 * np.pi * np.sqrt(L1 / g)
+    
+    return T0, T1
